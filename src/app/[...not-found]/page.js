97451d8880ba0792df17032f93f2ed0801691618
thecooolemail <thecooolemail@gmail.com>
2025-03-21 10:58:59 +0000

    1.1

---
+++ src/app/[...not-found]/page.js
@@ -0,0 +1,18 @@
+
+"use client"
+
+const Page = () => {
+
+
+    return(
+        <div >    
+                <div >
+                    <h3>404</h3>
+                    <p>Page Not Found</p>
+                </div>
+            
+        </div>
+    )
+}
+
+export default Page
